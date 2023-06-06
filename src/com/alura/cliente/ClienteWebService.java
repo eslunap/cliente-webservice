1ca0463e0b6a3f06bd8496cdebe7581a2c30706f
eslunap <estefany.luna@gmail.com>
2023-06-06 13:27:13 -0500

    configuración request

--- src/com/alura/cliente/ClienteWebService.java
+++ src/com/alura/cliente/ClienteWebService.java
@@ -1,10 +1,18 @@
 package com.alura.cliente;
 
+import org.apache.hc.client5.http.fluent.Request;
+
 public class ClienteWebService {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		
+		String contenido = Request.post("http://localhost:8080/gerenciador/empresas")
+				.addHeader("Accept", "Application/json")
+				.execute()
+				.returnContent()
+				.toString();
+		
+		System.out.println(contenido);
 	}
 
 }
